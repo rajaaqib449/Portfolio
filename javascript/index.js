41eb1c4c6d43414bdb1ce7a46fc1b9d8fd59109a
raja muhammad aqib <84813318+rajaaqib449@users.noreply.github.com>
2022-06-25 17:43:03 +0500

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+
+ function openSlideMenu(){
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('content').style.marginLeft = '250px';
+
+}
+function closeSideMenu(){
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('content').style.marginLeft = '0';
+    
+  
+    
+
+}
